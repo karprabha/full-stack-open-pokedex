ddd57bebb9af875549e3d7ea5125ec9ed2e307f7
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2024-04-13 00:10:38 +0530

    add e2e test

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
     node: true,
     es6: true,
     "jest/globals": true,
+    "cypress/globals": true,
   },
   extends: ["eslint:recommended", "plugin:react/recommended"],
   parserOptions: {
